e0ff8026487647b263549251afcf6677a0d27af9
zeh <victortelles18@gmail.com>
2024-05-18 23:40:31 -0300

    act: first ascent

---
+++ index.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow
+
+app.on('ready' , () => {
+
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 600,
+    resizable: false
+
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+});
